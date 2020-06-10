2df4296a52bf37a3d5c997aac842b14029cc7d1e
rafi67 <45172084+rafi67@users.noreply.github.com>
2020-06-10 12:21:03 +0600

    Updated

--- CGPAapp/app/src/main/java/com/softwaresolutions/cgpaapp/ShowDialog.java
+++ CGPAapp/app/src/main/java/com/softwaresolutions/cgpaapp/ShowDialog.java
@@ -11,14 +11,14 @@ public class ShowDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Information")
-                .setMessage(Error_Data.r)
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Error_Data e = new Error_Data(null);
-                    }
-                });
+            builder.setTitle("Information")
+                    .setMessage(Error_Data.r)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Error_Data e = new Error_Data(null);
+                        }
+                    });
         return builder.create();
     }
 }
